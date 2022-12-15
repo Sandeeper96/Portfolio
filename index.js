8321b52f2fa81140d2fc23fcd7d4e59b22cf094c
Sandeep kumar <sandeeper96@gmail.com>
2022-12-16 01:00:11 +0530

    I love u shraddha

---
+++ index.js
@@ -0,0 +1,9 @@
+const thembutton = document.querySelector('.nav-theme');
+const themeList = document.querySelector('.theme-list');
+
+thembutton.addEventListener('mouseover', show);
+
+function show(event) {
+  event.preventDefault();
+themeList.classList.add('hidden');
+}
